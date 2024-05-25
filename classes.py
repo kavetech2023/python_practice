e999fecbed5d34738b26598c9bd486026a81826c
kavetech203 <kavetech@gmail.com>
2024-05-25 22:13:37 +0300

    create classes

---
+++ classes.py
@@ -0,0 +1,12 @@
+class Dog():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def stash(self) :
+        print(self.x)   
+        print(self.y)
+
+yes = Dog()
+yes.stash
+       
