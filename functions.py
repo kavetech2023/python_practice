13aa69a6d49f00acda8f88c3cdf093a82b2acf65
kavetech2023 <kavetech@gmail.com>
2024-06-05 12:20:30 +0300

    create car order function

--- functions.py
+++ functions.py
@@ -9,5 +9,13 @@ def car():
     model = input("Enter car model: ")
     print("Your car model is: " + model)
     print("Would you like to place an order of your "+ model +"?")
+    answer = input("Please enter yes or no: ")
+    if answer == "yes":
+        print("Thanks for placing an order with us. Have a nice day!")
+        extras = input("Would you like to check out the extra features?: ")
+        if extras == "yes":
+            print("Please click the link to check out extra features")    
 
 car()    
+
+   
