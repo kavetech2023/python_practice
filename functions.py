b342014f6ed6d42e6708c4280d1887e2a4ba28ea
kavetech2023 <kavetech@gmail.com>
2024-06-05 11:53:39 +0300

    create new function car

--- functions.py
+++ functions.py
@@ -4,3 +4,10 @@ def puppy():
 
 
 puppy()    
+
+def car():
+    model = input("Enter car model: ")
+    print("Your car model is: " + model)
+    print("Would you like to place an order of your "+ model +"?")
+
+car()    
