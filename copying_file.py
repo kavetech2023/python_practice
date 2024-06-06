9677e079315af239493c8a84bc31e3666a6815f4
kavetech2023 <kavetech@gmail.com>
2024-06-06 14:36:37 +0300

    create copyting_file.py

---
+++ copying_file.py
@@ -0,0 +1,6 @@
+import shutil
+
+file_path = "C:\\Users\\kavet\\OneDrive\\Desktop\\text.txt"
+"""
+"""
+shutil.copyfile(file_path, 'copy2.txt')
---
+++ moving_file.py
@@ -0,0 +1,4 @@
+import os
+
+source = ""
+destination = ""
