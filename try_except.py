a03c1ef708d4bb06f33e971ad91b75490a4aa7ef
kavetech2023 <kavetech@gmail.com>
2024-06-06 11:38:07 +0300

    update try_except.py

--- try_except.py
+++ try_except.py
@@ -3,9 +3,17 @@
 try:
     numerator = int(input("Enter a number to divide: "))
     denominator = int(input("Enter a number to divide by: "))
-    result = numerator/denominator
-    print(result)                
-except ZeroDivisionError: # Handles Exceptions of divisions by zero
+    result = numerator/denominator                
+except ZeroDivisionError as e:# Handles Exceptions of divisions by zero
+    print(e) 
     print("You can't divide by zero :(  ")    
-except ValueError: # Handles Exceptions of divisions between numbers and non-numbers
+except ValueError as e: # Handles Exceptions of divisions between numbers and non-numbers
+    print(e) 
     print("You can only enter numbers :( ")    
+except Exception as e: #Handles all exception
+    print(e)
+    print("Something went wrong : ( ")    
+else:
+    print(result)    
+finally:
+    print("This will always excecute")    
