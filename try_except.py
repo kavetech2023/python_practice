3685c02db9f2e5b6b08657ba8e19ec1e2f046947
kavetech2023 <kavetech@gmail.com>
2024-06-06 11:20:25 +0300

    create try_except.py

---
+++ try_except.py
@@ -0,0 +1,7 @@
+"""exception = events detected during execution that interrupt
+               the flow of a program"""
+
+numerator = int(input("Enter a number to divide: "))
+denominator = int(input("Enter a number to divide by: "))
+result = numerator/denominator
+print(result)                
