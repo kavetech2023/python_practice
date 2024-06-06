4998cf47ce6fbb582b1323b50c54ffabfb03f240
kavetech2023 <kavetech@gmail.com>
2024-06-06 11:30:24 +0300

    update exceptions

--- try_except.py
+++ try_except.py
@@ -1,7 +1,11 @@
 """exception = events detected during execution that interrupt
                the flow of a program"""
-
-numerator = int(input("Enter a number to divide: "))
-denominator = int(input("Enter a number to divide by: "))
-result = numerator/denominator
-print(result)                
+try:
+    numerator = int(input("Enter a number to divide: "))
+    denominator = int(input("Enter a number to divide by: "))
+    result = numerator/denominator
+    print(result)                
+except ZeroDivisionError: # Handles Exceptions of divisions by zero
+    print("You can't divide by zero :(  ")    
+except ValueError: # Handles Exceptions of divisions between numbers and non-numbers
+    print("You can only enter numbers :( ")    
