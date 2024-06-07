148288c151f404d3d694c7a688ccf64e1d23f206
kavetech2023 <kavetech@gmail.com>
2024-06-07 10:20:35 +0300

    create walrus_operator.py

---
+++ walrus_operator.py
@@ -0,0 +1,20 @@
+# walrus operator :=
+# new to Python 3.8
+
+# Why use this operator?
+
+# (1) Code without walrus operator ->
+
+foods = list()
+while True:
+    food = input("What food do you like?: ")
+        if food == "quit":
+            break
+    foods.append(food)
+
+# (2) Code with walrus operator ->
+
+
+
+
+
