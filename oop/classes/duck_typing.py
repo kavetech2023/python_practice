941a57def755da767a6e198c1c41b5bfbbe43f2b
kavetech2023 <kavetech@gmail.com>
2024-06-07 09:50:08 +0300

    create duck_typing.py

---
+++ oop/classes/duck_typing.py
@@ -0,0 +1,4 @@
+# Duck typing = Concept where the class of an object is less important than the methods/attributes
+#               classtype is not checked if minimum methods/attributes are present.
+#               "If it walks like a duck, and it quacks like a duck then it is a duck"
+
