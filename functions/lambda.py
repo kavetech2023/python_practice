5a7f539ffda97845d9e9421e9ee98ac47d3f5632
kavetech2023 <kavetech@gmail.com>
2024-06-07 10:58:22 +0300

    order files into correct directories

---
+++ functions/lambda.py
@@ -0,0 +1,26 @@
+# lambda functions = functions written in 1 line using lambda keyword
+#                    accepts any number of arguments, but only has one expression
+#                    (think of it as a shortcut)
+#                    (useful if needed for a short period of time, throw-away)
+
+
+# syntax = lambda parameters: expression
+
+# (1) Without lambda #
+
+def double(x):
+    return x*2
+
+print(double)
+
+# (2) With lambda #
+
+double = lambda x : x * 2
+multiply = lambda x,y : x * y
+add = lambda x, y, z : x + y + z 
+
+print(double(5)) 
+print(multiply(5,7))
+print(add(6,7,8))
+
+
