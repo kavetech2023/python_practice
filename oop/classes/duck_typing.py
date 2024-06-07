40850e7838215c08cb1d1aaff25e972d1f900745
kavetech2023 <kavetech@gmail.com>
2024-06-07 10:15:43 +0300

    pass bmw class an argument

--- oop/classes/duck_typing.py
+++ oop/classes/duck_typing.py
@@ -2,3 +2,32 @@
 #               classtype is not checked if minimum methods/attributes are present.
 #               "If it walks like a duck, and it quacks like a duck then it is a duck"
 
+class Mazda:
+
+    
+    def go(self):
+        print("mazda is going")
+
+    def stop(self):
+        pprint("mazda has stopped")
+
+class Bmw:
+
+    def go(self):
+        print("Bmw has started")
+
+    def stop(self):
+        pass
+
+class Motorcycle:
+    def move(self, bmw):
+        bmw.stop()
+        bmw.go()
+        print("Now we are moving")
+
+
+mazda = Mazda()
+bmw = Bmw()
+motorcycle = Motorcycle()
+
+motorcycle.move(bmw) # Here i passed a CLASS as an argument
