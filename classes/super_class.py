923433185cfbb33b40e29572c719921f9fe257c2
kavetech2023 <kavetech@gmail.com>
2024-06-07 09:12:45 +0300

    create super class

---
+++ classes/super_class.py
@@ -0,0 +1,17 @@
+# super() = Function used to give access to the methods of a parent class.
+#           Returns a temporary object of a parent class when used.  
+
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+class Square(Rectangle):
+    def __init__(self,length,width):
+        super().__init__(length,width)
+
+
+class Cube(Rectangle):
+    def __init__(self,length,width,height):
+        super().__init__(length,width)
