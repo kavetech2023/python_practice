69de7249cd995a8087a9b2972923fac19cda965c
kavetech2023 <kavetech@gmail.com>
2024-06-14 23:01:12 +0300

    update comments

--- functions/lambda.py
+++ functions/lambda.py
@@ -3,24 +3,30 @@
 #                    (think of it as a shortcut)
 #                    (useful if needed for a short period of time, throw-away)
 
-
 # syntax = lambda parameters: expression
 
 # (1) Without lambda #
 
 def double(x):
-    return x*2
+    """
+    Doubles the given number.
+    
+    Parameters:
+    x (int): The number to be doubled.
+    
+    Returns:
+    int: The doubled number.
+    """
+    return x * 2
 
 print(double)
 
 # (2) With lambda #
 
-double = lambda x : x * 2
-multiply = lambda x,y : x * y
-add = lambda x, y, z : x + y + z 
+double = lambda x: x * 2
+multiply = lambda x, y: x * y
+add = lambda x, y, z: x + y + z
 
-print(double(5)) 
-print(multiply(5,7))
-print(add(6,7,8))
-
-
+print(double(5))
+print(multiply(5, 7))
+print(add(6, 7, 8))
