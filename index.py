6272cb6076f24430d8e5b378a52f1aed6c278c79
kavetech2023 <kavetech@gmail.com>
2024-06-28 23:19:01 +0300

    create index.py

---
+++ index.py
@@ -0,0 +1,9 @@
+num_one = int(input("Enter the first number: "))
+num_two = int(input("Enter the second number: "))
+
+sum = num_one + num_two 
+ 
+if sum == 10:
+    print("The sum is equal to 10")
+else:
+    print("The sum is not equal to 10")
